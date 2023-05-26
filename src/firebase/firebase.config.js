c31bd7b456f3a58861f9f20ab34337f68ff721da
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-05-26 12:12:41 +0600

    adding dotenv for security purpose of user credienttial data

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,14 +6,6 @@
 // // https://firebase.google.com/docs/web/setup#available-libraries
 
 // // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_APIKEY,
-//   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-//   projectId: import.meta.env.VITE_PROJECTID,
-//   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
-//   appId: import.meta.env.VITE_APPID,
-// };
 
 // // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
@@ -26,13 +18,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCzxnXcTm4ulcReIGZXWlwomXTAtJo8Y2s",
-  authDomain: "toy-shop-2730e.firebaseapp.com",
-  projectId: "toy-shop-2730e",
-  storageBucket: "toy-shop-2730e.appspot.com",
-  messagingSenderId: "508797985832",
-  appId: "1:508797985832:web:efa6fb51d4011780a0411d"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
