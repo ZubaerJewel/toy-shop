195cdfbefe3025d5431642b0698287574b381d2a
ZubaerJewel <zubaer5jewel.16@gmail.com>
2023-05-28 00:40:16 +0600

    set the error page without header and footer

--- src/components/MyToys.jsx
+++ src/components/MyToys.jsx
@@ -3,9 +3,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import ToyTable from "./ToyTable";
 import { AuthContest } from "../AuthProvider/AuthProvider";
+import PageNotFound from "./PageNotFound";
+import { useNavigate } from 'react-router-dom';
 
 const MyToys = () => {
+  const navigate = useNavigate();
   const [toys, setToys] = useState([]);
+  const [eror, setEror] = useState(false);
   const [isDelete, setIsDelete] = useState(false);
   const [sortAscending, setSortAscending] = useState(true);
   const [sort, setSort] = useState(1);
@@ -17,18 +21,58 @@ const MyToys = () => {
     setSortAscending(newSortOrder);
   };
 
+  // const handleError = () => {
+  //   navigate('*');
+  // };
+
   useEffect(() => {
-    fetch(
-      `https://education-toy-server.vercel.app/toys/${user?.email}?num=${sort}`
-    )
-      .then((res) => res.json())
-      .then((data) => setToys(data))
-      .catch((error) => console.log(error));
+    // fetch(
+    //   `https://education-toy-server.vercel.app/toys/${user?.email}?num=${sort}`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => setToys(data))
+    //   .catch((err) => console.log(err),
+    //   setError(true)
+    //   );
+   
+
+    const fetchData = async () => {
+      try {
+        // Simulating data fetching
+        const response = await fetch(`https://education-toy-server.vercel.app/toys/${user?.email}?num=${sort}`);
+        if (!response.ok) {
+          throw new Error('Data fetch failed');
+        }
+        const data = await response.json();
+        setToys(data);
+      } catch (error) { console.log(error);
+        setEror(true);
+      }
+    };
+
+    fetchData();
   }, [isDelete, sort]);
+
+  if(eror)
+  {
+    navigate('*');
+  }
+
+  // const fetchData = () => {
+  //   // Simulating a data fetching process
+  //   setTimeout(() => {
+  //     // const isSuccess = false; // Set this based on the success of the data fetch
+      
+  //     if (toys===0) {
+  //       navigate('*');
+  //     }
+  //   }, 3000);
+  // };
+
 
   return (
     <>
-      <div className="flex justify-center mt-4">
+    {toys ? (  <> <div className="flex justify-center mt-4">
         <span onClick={handleSortChange}>
           <button
             className={`mx-2 py-2 px-4 rounded focus:outline-none ${
@@ -64,7 +108,8 @@ const MyToys = () => {
             </tr>
           </thead>
           <tbody>
-            {toys &&
+            {
+            toys &&
               toys?.map((toy) => (
                 <ToyTable
                   key={toy._id}
@@ -76,14 +121,29 @@ const MyToys = () => {
               ))}
           </tbody>
         </table>
-      </div>
-      {toys.length == 0 && (
+      </div>  
+      </>
+  )
+
+: (
+  // Render loading state or fallback UI
+  <div>Loading...</div>
+)}
+      </>
+  
+  )  
+{/*     
+      {
+
+      
+      toys.length == 0 && (
         <p className="text-center text-4xl text-red-700 font-bold">
-          No toys found !
+          
+          
         </p>
-      )}
-    </>
-  );
+      )} */}
+    
+  
 };
 
 export default MyToys;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -80,6 +80,7 @@ const router = createBrowserRouter([
         path: "/signup",
         element: <SignUp />,
       },
+     
     ],
   },
   {
